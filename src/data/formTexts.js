04574e6478c1b6fcb694d9259a4925df09900679
Nikodem <cwikla.nikodem@gmail.com>
2023-03-04 01:51:23 +0100

    commit
    start procjet

---
+++ src/data/formTexts.js
@@ -0,0 +1,87 @@
+export const progressBar = [
+    { 
+      number: '1',
+      text: 'Wybierz rzeczy'
+    },
+    { 
+      number: '2',
+      text: 'Spakuj je w worki'
+    },
+    { 
+      number: '3',
+      text: 'Wybierz fundację'
+    },
+    { 
+      number: '4',
+      text: 'Zamów kuriera'
+    },
+  ]
+
+export const formVariables = [
+    {
+      info_bar:'Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.',
+      form_steps_title: 'Zaznacz co chcesz oddać:',
+    },
+    {
+      info_bar:'Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ.',
+      form_steps_title: 'Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:',
+    },
+    {
+      info_bar:'Jeśli wiesz komu chcesz pomóc, możesz wpisać nazwę tej organizacji w wyszukiwarce. Możesz też filtrować organizacje po ich lokalizacji bądź celu ich pomocy.',
+      form_steps_title: 'Lokalizacja:',
+    },
+    {
+      info_bar:'Podaj adres oraz termin odbioru rzeczy.',
+      form_steps_title: 'Podaj adres oraz termin odbioru rzecz przez kuriera',
+    },
+    {
+      info_bar: " ",
+      form_steps_title: 'Podsumowanie Twojej darowizny'
+    }
+  ]
+
+  export const choosenThings = [
+    {
+        id: 'reuse-clothes',
+        label: 'ubrania w dobrym stanie'
+    },
+    {
+        id: 'trash-clothes',
+        label: 'ubrania do wyrzucenia'
+    },
+    {
+        id: 'toys',
+        label: 'zabawki'
+    },
+    {
+        id: 'books',
+        label: 'książki'
+    },
+    {
+        id: 'others',
+        label: 'inne'
+    }
+]
+
+export const helpGroups = [
+  {
+    id: 'children',
+    label: 'dzieciom'
+  }, 
+  {
+    id: 'single-mothers',
+    label:'samotnym matkom'
+  }, 
+  {
+    id:'homeless',
+    label:'bezdomnym'
+  }, 
+  {
+    id: 'disabled-people',
+    label:'niepełnosprawnym'
+  }, 
+  {
+    id:'elder-people', 
+    label:'osobom starszym'
+  }
+]
